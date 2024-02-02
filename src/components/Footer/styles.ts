552299ce4edb0c5a7c112fc91ebeecfe7af3d7c2
miguel11065 <miguel11065@gmail.com>
2024-02-02 12:51:42 -0300

    Arrumando o footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components'
 
 export const FooterBar = styled.footer`
-  position: absolute;
-  bottom: 0;
   width: 100%;
   background-color: #20b2aa;
   color: #663399;
